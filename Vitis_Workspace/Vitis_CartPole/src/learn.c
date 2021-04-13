e14258ac5ed35a02f201a4418f7716296762d117
kmfrick <kmfrick98@gmail.com>
2021-04-14 01:04:34 +0200

    Vitis_CartPole/src/learn.c: fix var types

--- Vitis_Workspace/Vitis_CartPole/src/learn.c
+++ Vitis_Workspace/Vitis_CartPole/src/learn.c
@@ -57,12 +57,12 @@
 #define N_AGENTS 2
 
 int main() {
-	int i, j;
+	int i, j, k;
 	u32 is_done, return_val, all_done;
 	u16 device_id = 0;
 	XLearn instance_ptr[N_AGENTS];
 	int error;
-	char failures[N_AGENTS];
+	short failures;
 	useconds_t sleeping_time_us = 5000000;
 
 	init_platform();
@@ -72,57 +72,67 @@ int main() {
 		XLearn_LookupConfig(device_id);
 		error = XLearn_Initialize(&instance_ptr[device_id], device_id);
 		if (!error) {
-			printf("Init %d OK! Base addr = %p\n", device_id, instance_ptr[device_id].Axilites_BaseAddress);
+			printf("\nInit %d OK! Base addr = %p\n", device_id,
+					instance_ptr[device_id].Axilites_BaseAddress);
 		} else {
-			printf("Init %d failed!\n", device_id);
+			printf("\nInit %d failed!\n", device_id);
 		}
 	}
 
-	for (device_id = 0; device_id < N_AGENTS; device_id++) {
-		XLearn_Start(&instance_ptr[device_id]);
-		printf("Agent %d starting\n", device_id);
+	short zero = 0;
+	for (k = 0; k < N_AGENTS; k++) {
+		XLearn_Write_failures_Bytes(&instance_ptr[k], 0, &zero,
+								sizeof(short));
 		for (i = 0; i < N_BOXES; i++) {
 			for (j = 0; j < N_ACTIONS; j++) {
 				float r = (rand() % 100) / 100.0f;
 				if (i == 0 && j == 0) {
-					printf("q%d00 = %.3f\n", device_id, r);
+					printf("q%d00 = %.3f\n", k, r);
 				}
-				XLearn_Write_q_shared_Words(&instance_ptr[device_id], i * N_ACTIONS + j, (int*)(&r), 1);
+					XLearn_Write_q_shared_Words(&instance_ptr[k],
+						k * N_ACTIONS * N_BOXES + i * N_ACTIONS + j,
+						(int*) (&r), 1);
 			}
 		}
+	}
+	printf("\nfailures, q initialized\n");
 
-		XLearn_Write_failures_Bytes(&instance_ptr[device_id], 0, 0, sizeof(char));
+	for (device_id = 0; device_id < N_AGENTS; device_id++) {
+		XLearn_Start(&instance_ptr[device_id]);
+		printf("Agent %d starting\n", device_id);
 	}
 	all_done = 0;
-	usleep(sleeping_time_us);
 	while (!all_done) {
+		usleep(sleeping_time_us);
 		all_done = 1;
 		for (device_id = 0; device_id < N_AGENTS; device_id++) {
 			is_done = XLearn_IsDone(&instance_ptr[device_id]);
-			printf("Agent %d done = %lu\n", device_id, is_done);
-			return_val  = XLearn_ReadReg(instance_ptr->Axilites_BaseAddress, XLEARN_AXILITES_ADDR_AP_CTRL);
-			printf("AP_CTRL = %lu\n", return_val);
 			if (!is_done) {
 				all_done = 0;
-				usleep(sleeping_time_us);
 			}
-			float r;
-			XLearn_Read_q_shared_Words(&instance_ptr[device_id], 0, &r, 1);
-			printf("q%d00 = %.3f\n", device_id, r);
+			printf("Agent %d done = %lu\n", device_id, is_done);
+			//is_done  = XLearn_ReadReg(instance_ptr->Axilites_BaseAddress, XLEARN_AXILITES_ADDR_AP_CTRL);
+			//printf("AP_CTRL = %lu\n", is_done);
+			//float r;
+			//XLearn_Read_q_shared_Words(&instance_ptr[device_id], 0, &r, 1);
+			//printf("q%d00 = %.3f\n", device_id, r);
 		}
 	}
-	printf("Printing return values\n");
+	printf("\nPrinting return values\n");
 	for (device_id = 0; device_id < N_AGENTS; device_id++) {
 		return_val = XLearn_Get_return(&instance_ptr[device_id]);
 		printf("return = %lu\n", return_val);
 	}
-	printf("Printing periods\n");
+	printf("\nPrinting periods\n");
 	for (device_id = 0; device_id < N_AGENTS; device_id++) {
-		XLearn_Read_failures_Words(&instance_ptr[device_id], 0, &failures[device_id], 1);
-		printf("Agent %d failures = %d\n", device_id, failures[device_id]);
+		for (i = 0; i < N_AGENTS; i++) {
+			XLearn_Read_failures_Bytes(&instance_ptr[device_id],
+					i * sizeof(short), &failures, sizeof(short));
+			printf("Agent %d failures = %d\n", i, failures);
+		}
 	}
 
-	printf("Goodbye!\n");
+	printf("\nGoodbye!\n");
 	cleanup_platform();
 	return 0;
 }
