385a4b0d193e197585ecb80d88edc126c7f289f4
davideraga <56338910+davideraga@users.noreply.github.com>
2022-03-06 20:50:31 +0100

    removed unused variables

--- HLS_CartPole/pole.cpp
+++ HLS_CartPole/pole.cpp
@@ -300,7 +300,6 @@ int learn(random_stream &hls_rand_stream,  status_bits &running,   qtable q[N_AG
 
 	int failures[N_AGENTS];
 	running = STATUS_START;
-	float p, oldp, rhat, r;
 	int failed;
 	int action;
 	int state;
